a85f482483714f823e372c5da6244e62e706bdcb
sdsykes <sdsykes>
2004-07-02 12:19:05 +0000

    Refactored sysids

--- lib/madeleine/automatic.rb
+++ lib/madeleine/automatic.rb
@@ -19,8 +19,9 @@ module Madeleine
 # automatic_read_only, and its opposite automatic_read_write.
 #
 # Should you require it, the snapshots can be stored as yaml, and can be compressed.  Just pass 
-# the marshaller you want to use as the second argument to AutomaticSnapshotMadeleine.new.  The system 
-# will automatically detect and read either Marshal, compressed Marshal or YAML at startup.
+# the marshaller you want to use as the second argument to AutomaticSnapshotMadeleine.new.  
+# If the passed marshaller did not successfully deserialize the latest snapshot, the system 
+# will try to automatically detect and read either Marshal, YAML, or compressed Marshal or YAML.
 #
 # This module is designed to work correctly in the case there are multiple madeleine systems in use by
 # a single program, and is also safe to use with threads.
@@ -118,23 +119,16 @@ module Madeleine
 # A Command object is automatically created for each method call to an object within the system that comes from without.
 # These objects are recorded in the log by Madeleine.
 # 
-# Note: The command also records which system it belongs to.  This is used in a recovery situation.
-# If a command contains a sysid that doesn't match the system sent to us, then we change that
-# system's id to the one in the command.  This makes a system adopt the correct id as soon as a
-# command for it is executed.  This is the case when restoring a system for which there is no snapshot.
-#
     class Command
-      def initialize(symbol, myid, sysid, *args)
+      def initialize(symbol, myid, *args)
         @symbol = symbol
         @myid = myid
-        @sysid = sysid
         @args = args
       end
 #
 # Called by madeleine when the command is done either first time, or when restoring the log
 #
       def execute(system)
-        AutomaticSnapshotMadeleine.register_sysid(@sysid) if (system.sysid != @sysid)
         Thread.current[:system].myid2ref(@myid).thing.send(@symbol, *@args)
       end
     end
@@ -182,7 +176,7 @@ module Madeleine
           raise "Cannot make command with block" if block_given?
           Thread.current[:system] = AutomaticSnapshotMadeleine.systems[@sysid]
           begin
-            result = Thread.current[:system].execute_command(Command.new(symbol, @myid, @sysid, *args))
+            result = Thread.current[:system].execute_command(Command.new(symbol, @myid, *args))
           ensure
             Thread.current[:system] = false
           end
@@ -190,7 +184,7 @@ module Madeleine
         end
       end
 #
-# Custom marshalling - this adds the internal id (myid) and the system id to a marshall 
+# Custom marshalling - this adds the internal id (myid) and the system id to a marshal
 # of the object we are the proxy for.
 # We take care to not marshal the same object twice, so circular references will work.
 # We ignore Thread.current[:system].marshaller here - this is only called by Marshal, and
@@ -202,10 +196,10 @@ module Madeleine
             [@myid.to_s, @sysid].pack("A8A30")
           else
             Thread.current[:snapshot_memory][self] = true
-           [@myid.to_s, @sysid].pack("A8A30") + Marshal.dump(@thing, depth)
+            [@myid.to_s, @sysid].pack("A8A30") + Marshal.dump(@thing, depth)
           end
         else
-          [@myid.to_s, @sysid].pack("A8A30")
+          [@myid.to_s, @sysid].pack("A8A30")  # never marshal a prox object in a command, just ref
         end
       end
 #
@@ -234,19 +228,29 @@ module Madeleine
 # marshalling will work correctly.
 #
     class AutomaticSnapshotMadeleine
-      attr_accessor :sysid, :marshaller
-      attr_reader :list, :marshaller
+      attr_accessor :marshaller
+      attr_reader :list, :sysid
 
       def initialize(directory_name, marshaller=Marshal, persister=SnapshotMadeleine, &new_system_block)
         @sysid ||= Time.now.to_f.to_s + Thread.current.object_id.to_s # Gererate a new sysid
-        @myid_count = 0                                               # This sysid will be used only if new
-        @list = {}                                                    # object is taken by madeleine
+        @myid_count = 0
+        @list = {}
         Thread.current[:system] = self # during system startup system should not create commands
-#        AutomaticSnapshotMadeleine.register_sysid(@sysid) # this sysid may be overridden
+        Thread.critical = true
+        @@systems ||= {}  # holds systems by sysid
+        @@systems[@sysid] = self
+        Thread.critical = false
         @marshaller = marshaller # until attrb
         begin
           @persister = persister.new(directory_name, Automatic_marshaller, &new_system_block)
-          AutomaticSnapshotMadeleine.register_sysid(@sysid) # needed if there were no commands
+          @@systems[@sysid].list.delete_if {|k,v|  # set all the prox objects that now exist to have the right sysid
+            begin
+              ObjectSpace._id2ref(v).sysid = @sysid
+              false
+            rescue RangeError
+              true # Id was to a GC'd object, delete it
+            end
+          }
         ensure
           Thread.current[:system] = false
         end
@@ -263,13 +267,12 @@ module Madeleine
 # If the object already exists in the system then the existing object must be used.
 #
       def restore(proxo)  
-        if (@list[proxo.myid])# && proxo.sysid == myid2ref(proxo.myid).sysid) 
+        if (@list[proxo.myid])
           proxo = myid2ref(proxo.myid)
         else
           @list[proxo.myid] = proxo.object_id
           @myid_count = proxo.myid if (@myid_count < proxo.myid)
         end
-        @sysid = proxo.sysid # to be sure to have the correct sysid
         proxo
       end
 #
@@ -291,24 +294,6 @@ module Madeleine
           Thread.current[:snapshot_memory] = nil
           Thread.current[:system] = false
         end
-      end
-#
-# Sets the real sid for this thread's system - called during startup or from a command.
-#
-      def AutomaticSnapshotMadeleine.register_sysid(sid)
-        Thread.critical = true
-        @@systems ||= {}  # holds systems by sysid
-        @@systems[sid] = Thread.current[:system]
-        Thread.critical = false
-        @@systems[sid].sysid = sid
-        @@systems[sid].list.delete_if {|k,v|  # set all the prox objects that already exist to have the right sysid
-          begin
-            ObjectSpace._id2ref(v).sysid = sid
-            false
-          rescue RangeError
-            true # Id was to a GC'd object, delete it
-          end
-        }
       end
 #
 # Returns the hash containing the systems. 
@@ -381,7 +366,7 @@ module Madeleine
             if (detected_zmarshaller)
               ZMarshal.new(detected_zmarshaller).load(io)
             else
-              marshaller.load(io)
+              raise e
             end
           elsif (detected_marshaller)
             detected_marshaller.load(io)
